d1de0ed596d8c0a71cd0c6bb1c3e48cc57558067
sondaydungso <tranphong2k3@gmail.com>
2023-09-19 10:02:37 +1000

    change code
    still bug but it is the better version

--- custom_pro.rb
+++ custom_pro.rb
@@ -5,18 +5,26 @@ module ZOrder
   BACKGROUND, MIDDLE, TOP = *0..2
 end
 class Ultra < Gosu::Window
+	attr_accessor :back
 	def initialize()
 		super(800,600)
 		self.caption = "Sharpen your reflecion!"
 		@radius = 20
 		@dot = Gosu::Image.new('dot.png')
-		@x = 310
-		@y = 240
+		@back = Gosu::Image.new('yellow.jpg')
+		@x = 300
+		@x_back = 300
+		@y = 300
+		@y_back = 300
 		@width = 1
 		@height = 1
 		@velocity_x = 2
+		@velocity_backx = 5
 		@velocity_y = 2
+		@velocity_backy = 5
 		@visible = 0
+		@visible_back = 0
+
 		#@hammer_image = Gosu::Image.new('hammer.png')
 		@hit = 0 
 		@score = 0
@@ -30,17 +38,31 @@ class Ultra < Gosu::Window
 
 	end
 	def update()
-
-		
-		
-
-
+		@x += @velocity_x
+		@y += @velocity_y
+		@velocity_x *= -1 if @x - @width / 2 > 590 || @x - @width < 210
+		@velocity_y *= -1 if @y - @height /2 > 390 || @y - @height < 210
+		@x_back += @velocity_backx
+		@y_back += @velocity_backy
+		@velocity_backx *= -1 if @x_back - @width / 2 > 590 || @x_back - @width < 210
+		@velocity_backy *= -1 if @y_back - @height /2 > 390 || @y_back - @height < 210
+		@visible -= 1
+		@visible_back -= 1
+		@visible = 30 if @visible < -10 and rand 0.5
+		@visible_back = 10 if @visible < -10 and rand 0.7
 
 	end
 	def draw()
 		draw_quad(0, 0, 0xffffffff, 0, 600, 0xffffffff, 800, 600, 0xffffffff, 800, 0, 0xffffffff, 0)
-		draw_quad(200,200, Gosu::Color::BLUE, 600, 200, Gosu::Color::BLUE, 600, 400, Gosu::Color::BLUE, 200, 400, Gosu::Color::BLUE, 0)
-    @dot.draw(300,300)
+		draw_quad(200,200, Gosu::Color::GREEN, 600, 200, Gosu::Color::GREEN, 600, 400, Gosu::Color::GREEN, 200, 400, Gosu::Color::GREEN, ZOrder::BACKGROUND)
+    #ack = draw_quad(200,200, Gosu::Color::GREEN, 600, 200, Gosu::Color::GREEN, 600, 400, Gosu::Color::GREEN, 200, 400, Gosu::Color::GREEN, ZOrder::MIDDLE)
+    if @visible > 0
+ 			@dot.draw(@x,@y, ZOrder::TOP)
+ 			@back.draw(@x,@y, ZOrder::MIDDLE)
+ 		end
+ 		
+
+
 		
 			
 	end
